3dab945ceec168fe69ef243deb1dd713d5b1e5d5
guilhermerubrae <guilhermerubrae@gmail.com>
2024-01-31 16:13:10 -0300

    model repository controller final

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,11 +1,20 @@
 package com.generation.farmacia.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -19,6 +28,55 @@ public class Categoria {
 	@NotBlank(message = "Nome não pode ficar vazio")
 	@Size(min = 4, message = "Nome deve conter minímo de 4 letras")
 	private String nome;
+
+	@NotNull
+	private String tipo;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull(message = "Preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior do que zero!")
+	private BigDecimal preco;
+	
+	@Column(name = "validade")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private LocalDate validade;
+	
+	@AssertTrue
+	@NotNull(message = "Prescrição médica é obrigatória!")
+	private boolean requerPrescricao;
+
+
+	public boolean isRequerPrescricao() {
+		return requerPrescricao;
+	}
+
+	public void setRequerPrescricao(boolean requerPrescricao) {
+		this.requerPrescricao = requerPrescricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public LocalDate getValidade() {
+		return validade;
+	}
+
+	public void setValidade(LocalDate validade) {
+		this.validade = validade;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 	private String descricao;
 
